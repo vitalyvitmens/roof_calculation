f65cc254e45932367c10a8633de5e90fe8c4d909
vitalyvitmens <otm_@tut.by>
2022-06-30 13:48:37 +0300

    порефачил код (изменил основной класс, добавил еще один через наследование, начал выносить логику из принтов в отдельные переменные)

--- main.py
+++ main.py
@@ -1,82 +1,106 @@
 import math
 
-USEFUL_WIDTH_OF_THE_METAL_TILE: float = 1.10
-FULL_WIDTH_OF_THE_METAL_TILE: float = 1.19
+USEFUL_WIDTH_METAL_TILE: float = 1.10
+FULL_WIDTH_METAL_TILE: float = 1.19
 
 
 def int_r(num: float):
+    """
+    Статическая функция int_r, округляет в меньшую сторону, если число после запятой < 0.5,
+    а так же округляет в большую сторону, если число после запятой > 0.5.
+    На вход принимает параметр num (число с плавающей точкой) и возвращает int num
+    """
     num = int(num + (0.5 if num > 0 else -0.5))
     return num
 
 
-class GableRoof:
+class RoofSlope:
     def __init__(self,
-                 skate_bar: float,
-                 near_cornice_bar: float,
-                 far_cornice_bar: float,
-                 near_left_end_plate: float,
-                 near_right_end_plate: float,
-                 far_left_end_plate: float,
-                 far_right_end_plate: float,
-                 cornice_overhang: float = 0.05,
+                 ridge: float,
+                 cornice: float,
+                 left_end: float,
+                 right_end: float,
+                 cornice_clearance: float = 0.05,
                  ridge_clearance: float = 0.0,
                  ):
         """
-        Класс GableRoof (двухскатная крыша), включает в себя следующие параметры:
-        skate_bar: коньковая планка
-        near_cornice_bar: ближняя карнизная планка
-        far_cornice_bar: дальняя карнизная планка
-        near_left_end_plate: ближняя левая торцевая планка
-        near_right_end_plate: ближняя правая торцевая планка
-        far_left_end_plate: дальняя левая торцевая планка
-        far_right_end_plate: дальняя правая торцевая планка
-        cornice_overhang: карнизный свес
-        ridge_clearance: коньковый напуск (по умолчанию равен нулю)
+        Класс RoofSlope (кровельный скат), включает в себя следующие параметры:
+        ridge: конёк - верхняя сторона прямоугольника в единицах измерения мп
+        cornice: карниз - нижняя сторона прямоугольника в единицах измерения мп
+        left_end: левый торец - левая сторона прямоугольника в единицах измерения мп
+        right_end: правый торец - правая сторона прямоугольника в единицах измерения мп
+        cornice_clearance: карнизный напуск - на какую величину в мп, выходят листы
+        металлочерепицы за край карниза
+        ridge_clearance: коньковый напуск - на какую величину в мп, выходят листы
+        металлочерепицы выше каря конька (по умолчанию равен нулю)
         """
-        self.skate_bar = skate_bar
-        self.near_cornice_bar = near_cornice_bar
-        self.far_cornice_bar = far_cornice_bar
-        self.near_left_end_plate = near_left_end_plate
-        self.near_right_end_plate = near_right_end_plate
-        self.far_left_end_plate = far_left_end_plate
-        self.far_right_end_plate = far_right_end_plate
-        self.cornice_overhang = cornice_overhang
+        self.ridge = ridge
+        self.cornice = cornice
+        self.left_end = left_end
+        self.right_end = right_end
+        self.cornice_clearance = cornice_clearance
         self.ridge_clearance = ridge_clearance
 
-    def number_of_ramp_sheets_1(self):
-        return self.near_cornice_bar / USEFUL_WIDTH_OF_THE_METAL_TILE
+    def number_sheets(self):
+        return round(self.cornice / USEFUL_WIDTH_METAL_TILE, 2)
 
-    def length_of_the_ramp_sheets_1(self):
-        return self.near_left_end_plate + self.cornice_overhang + self.ridge_clearance
-
-    def number_of_ramp_sheets_2(self):
-        return round(self.far_cornice_bar / USEFUL_WIDTH_OF_THE_METAL_TILE, 2)
-
-    def length_of_the_ramp_sheets_2(self):
-        return self.far_left_end_plate + self.cornice_overhang + self.ridge_clearance
+    def length_sheets(self):
+        return self.left_end + self.cornice_clearance + self.ridge_clearance
 
 
-slope1 = GableRoof(10, 10, 10, 5, 5, 5, 5, 0.05)
-slope2 = GableRoof(10, 10, 10, 5, 5, 5, 5, 0)
-roof_area = slope1.near_cornice_bar * slope1.near_left_end_plate + slope2.far_cornice_bar * slope2.far_left_end_plate
-print(f'Длина листов ската №1:         {round(slope1.length_of_the_ramp_sheets_1() * 1000)}мм\n'
-      f'Количество листов МЧ ската №1: {int_r(slope1.number_of_ramp_sheets_1())}шт')
-print(f'Длина листов ската №2:         {round(slope2.length_of_the_ramp_sheets_2() * 1000)}мм\n'
-      f'Количество листов МЧ ската №2: {int_r(slope2.number_of_ramp_sheets_2())}шт')
-print(f'Итого листов по двум скатам:   {round(slope1.number_of_ramp_sheets_1() + slope2.number_of_ramp_sheets_2())}шт')
-print(f'S кровли:                      {round(roof_area, 3)}м2')
-print(f'Полезной S металлочерепицы:    {round(slope1.number_of_ramp_sheets_1() * slope1.length_of_the_ramp_sheets_1() * USEFUL_WIDTH_OF_THE_METAL_TILE, 3)}м2')
-print(f'Полной S металлочерепицы:      {round(slope1.number_of_ramp_sheets_1() * slope1.length_of_the_ramp_sheets_1() * FULL_WIDTH_OF_THE_METAL_TILE, 3)}м2')
+class RoofSlope2(RoofSlope):
+    def __init__(self,
+                 ridge: float,
+                 cornice: float,
+                 left_end: float,
+                 right_end: float,
+                 cornice_clearance: float = 0.05,
+                 ridge_clearance: float = 0.0,
+                 ):
+        """
+        Класс RoofSlope2 (кровельный скат №2), включает в себя следующие параметры:
+        ridge: конёк - верхняя сторона прямоугольника в единицах измерения мп ската №2
+        cornice: карниз - нижняя сторона прямоугольника в единицах измерения мп ската №2
+        left_end: левый торец - левая сторона прямоугольника в единицах измерения мп ската №2
+        right_end: правый торец - правая сторона прямоугольника в единицах измерения мп ската №2
+        cornice_clearance: карнизный напуск - на какую величину в мп, выходят листы
+        металлочерепицы за край карниза ската №2
+        ridge_clearance: коньковый напуск - на какую величину в мп, выходят листы
+        металлочерепицы выше каря конька  ската №2 (по умолчанию равен нулю)
+        """
+        super().__init__(ridge, cornice, left_end, right_end, cornice_clearance, ridge_clearance)
+
+
+slope1 = RoofSlope(10, 10, 5, 5, 0, 0.05)
+useful_metal_tile_area1 = slope1.number_sheets() * slope1.length_sheets() * USEFUL_WIDTH_METAL_TILE
+full_metal_tile_area1 = slope1.number_sheets() * slope1.length_sheets() * FULL_WIDTH_METAL_TILE
+
+slope2 = RoofSlope2(10, 10, 5, 5, 0, 0)
+useful_metal_tile_area2 = slope2.number_sheets() * slope2.length_sheets() * USEFUL_WIDTH_METAL_TILE
+full_metal_tile_area2 = slope2.number_sheets() * slope2.length_sheets() * FULL_WIDTH_METAL_TILE
+
+roof_area = round(slope1.cornice * slope1.left_end + slope2.cornice * slope2.left_end, 3)
+useful_metal_tile_area = round(useful_metal_tile_area1 + useful_metal_tile_area2, 3)
+full_metal_tile_area = round(full_metal_tile_area1 + full_metal_tile_area2, 3)
+
+print(f'Длина листов ската №1:         {round(slope1.length_sheets() * 1000)}мм\n'
+      f'Количество листов МЧ ската №1: {int_r(slope1.number_sheets())}шт')
+print(f'Длина листов ската №2:         {round(slope2.length_sheets() * 1000)}мм\n'
+      f'Количество листов МЧ ската №2: {int_r(slope2.number_sheets())}шт')
+print(f'Итого листов по двум скатам:   {round(slope1.number_sheets() + slope2.number_sheets())}шт')
+print(f'S кровли:                      {roof_area}м2')
+print(f'Полезной S металлочерепицы:    {useful_metal_tile_area}м2')
+print(f'Полной S металлочерепицы:      {full_metal_tile_area}м2')
 print(f'Гидроизоляционная пленка:      {math.ceil(roof_area / 75)}рул = {math.ceil(roof_area / 75) * 75}м2')
 print(f'Мембрана:                      {math.ceil(roof_area / 75)}рул = {math.ceil(roof_area / 75) * 75}м2')
 print(f'Пароизоляционная пленка:       {math.ceil(roof_area / 70)}рул = {math.ceil(roof_area / 70) * 70}м2')
 print(f'Саморезы кровельные 4,8х29мм:  {math.ceil((roof_area * 8) / 250)}уп = {(math.ceil((roof_area * 8) / 250) * 250)}шт')
-print(f'Саморезы коньковые 4,8х70мм:   {math.ceil((slope1.skate_bar * 6) / 100)}уп = {(math.ceil((slope1.skate_bar * 6) / 100) * 100)}шт')
-print(f'Лента коньковая 260мм х 5м:    {math.ceil(slope1.skate_bar / 5)}шт = {math.ceil(slope1.skate_bar)}мп')
-print(f'Конек 150:                     {math.ceil((slope1.skate_bar * 1.05) / 2)}шт = {(math.ceil((slope1.skate_bar * 1.05) / 2)) * 2}мп')
-print(f'Конек цилиндрический:          {math.ceil((slope1.skate_bar * 1.05) / 1.2)}шт = {(math.ceil((slope1.skate_bar * 1.05) / 1.2)) * 1.25}мп')
-print(f'Заглушка простая:              {int(slope1.skate_bar / slope1.skate_bar) * 2}шт')
-print(f'Карнизная планка:              {(math.ceil((slope1.near_cornice_bar * 1.05) / 2)) + (math.floor((slope2.far_cornice_bar * 1.05) / 2))}шт = {((math.ceil((slope1.near_cornice_bar * 1.05) / 2)) + (math.floor((slope2.far_cornice_bar * 1.05) / 2))) * 2}мп')
-print(f'L-планка:                      {(math.ceil((slope1.near_cornice_bar * 1.05) / 2)) + (math.floor((slope2.far_cornice_bar * 1.05) / 2))}шт = {((math.ceil((slope1.near_cornice_bar * 1.05) / 2)) + (math.floor((slope2.far_cornice_bar * 1.05) / 2))) * 2}мп')
-print(f'Торцевая планка:               {int_r(((slope1.near_left_end_plate * 1.05) / 2) + ((slope2.near_right_end_plate * 1.05) / 2) + ((slope1.far_left_end_plate * 1.05) / 2) + ((slope2.far_right_end_plate * 1.05) / 2))}шт = {int_r(((slope1.near_left_end_plate * 1.05) / 2) + ((slope2.near_right_end_plate * 1.05) / 2) + ((slope1.far_left_end_plate * 1.05) / 2) + ((slope2.far_right_end_plate * 1.05) / 2)) * 2}мп')
-print(f'Планка снегозадержателя:       {int((math.ceil(slope1.near_cornice_bar) / 2) + (math.floor(slope2.far_cornice_bar) / 2))}шт = {int((math.ceil(slope1.near_cornice_bar) / 2) + (math.floor(slope2.far_cornice_bar) / 2)) * 2}мп')
+print(f'Саморезы коньковые 4,8х70мм:   {math.ceil((slope1.ridge * 6) / 100)}уп = {(math.ceil((slope1.ridge * 6) / 100) * 100)}шт')
+print(f'Лента коньковая 260мм х 5м:    {math.ceil(slope1.ridge / 5)}шт = {math.ceil(slope1.ridge)}мп')
+print(f'Конек 150:                     {math.ceil((slope1.ridge * 1.05) / 2)}шт = {(math.ceil((slope1.ridge * 1.05) / 2)) * 2}мп')
+print(f'Конек цилиндрический:          {math.ceil((slope1.ridge * 1.05) / 1.2)}шт = {(math.ceil((slope1.ridge * 1.05) / 1.2)) * 1.25}мп')
+print(f'Заглушка простая:              {int(slope1.ridge / slope1.ridge) * 2}шт')
+print(f'Карнизная планка:              {(math.ceil((slope1.cornice * 1.05) / 2)) + (math.floor((slope2.cornice * 1.05) / 2))}шт = {((math.ceil((slope1.cornice * 1.05) / 2)) + (math.floor((slope2.cornice * 1.05) / 2))) * 2}мп')
+print(f'L-планка:                      {(math.ceil((slope1.cornice * 1.05) / 2)) + (math.floor((slope2.cornice * 1.05) / 2))}шт = {((math.ceil((slope1.cornice * 1.05) / 2)) + (math.floor((slope2.cornice * 1.05) / 2))) * 2}мп')
+print(f'Торцевая планка:               {int_r(((slope1.left_end * 1.05) / 2) + ((slope2.right_end * 1.05) / 2) + ((slope1.left_end * 1.05) / 2) + ((slope2.right_end * 1.05) / 2))}шт = {int_r(((slope1.left_end * 1.05) / 2) + ((slope2.right_end * 1.05) / 2) + ((slope1.left_end * 1.05) / 2) + ((slope2.right_end * 1.05) / 2)) * 2}мп')
+print(f'Планка снегозадержателя:       {int((math.ceil(slope1.cornice) / 2) + (math.floor(slope2.cornice) / 2))}шт = {int((math.ceil(slope1.cornice) / 2) + (math.floor(slope2.cornice) / 2)) * 2}мп')
